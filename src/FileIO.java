d5051dfe16a0375a7575f8e13d4a5fbb4ddc5a22
LoganN011 <nunnologan@gmail.com>
2024-10-21 21:54:11 -0600

    built method to read in the grid and updated the cases to give the size of the graph

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,3 +1,51 @@
+import java.util.Scanner;
+
 public class FileIO {
-    //TODO:Make read in file either from command line args or console
+    public static GraphNode[][] buildGraph(String fileName) {
+        Scanner sc = new Scanner(fileName);
+        return getGraphNodes(sc);
+    }
+
+    public static GraphNode[][] buildGraph() {
+        Scanner sc = new Scanner(System.in);
+        return getGraphNodes(sc);
+    }
+
+    private static GraphNode[][] getGraphNodes(Scanner sc) {
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        GraphNode[][] matrix = new GraphNode[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            String line = sc.next();
+            for (int j = 0; j < cols; j++) {
+                if (line.charAt(j) == '0') {
+                    matrix[i][j] = new GraphNode(i, j, "LABEL??", false);
+                } else if (line.charAt(j) == 'I') {
+                    matrix[i][j] = new GraphNode(i, j, "LABEL??", true);
+                }
+            }
+        }
+        return matrix;
+    }
+
+    public static void printGraph(GraphNode[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == null) {
+                    System.out.print("1");
+                } else if (matrix[i][j].hasItem()) {
+                    System.out.print("I");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        GraphNode[][] matrix = buildGraph();
+        printGraph(matrix);
+    }
 }
