1eb910aa8da21baa7f30f1d9aa05117fcd454f6c
LoganN011 <nunnologan@gmail.com>
2024-11-20 22:34:17 -0700

    added new grids for testing constant size

--- src/FileIO.java
+++ src/FileIO.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class FileIO {
     public static long startTime = -1;
     public static long numberNodesVisited = 0;
+    public static int numberNodes = 0;
+    public static int numberItems = 0;
+    public static int numberOfHoles = 0;
+
     public static GraphNode[][] buildGraph(String fileName) {
         Scanner sc = new Scanner(fileName);
         return getGraphNodes(sc);
@@ -20,14 +24,36 @@ public class FileIO {
         for (int i = 0; i < rows; i++) {
             String line = sc.next();
             for (int j = 0; j < cols; j++) {
+                numberNodes++;
                 if (line.charAt(j) == '0') {
                     matrix[i][j] = new GraphNode(i, j, "LABEL??", false);
                 } else if (line.charAt(j) == 'I') {
                     matrix[i][j] = new GraphNode(i, j, "LABEL??", true);
+                    numberItems++;
+                } else {
+                    numberOfHoles++;
                 }
             }
         }
+
         return matrix;
+    }
+
+    public static void getNumberOfEdges(GraphNode[][] matrix) {
+        int count = 0;
+        int[] rowMove = {1, -1, 0, 0};
+        int[] colMove = {0, 0, 1, -1};
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                for (int k = 0; k < 4; k++) {
+                    if (GraphNode.isValid(matrix, i + rowMove[k], j + colMove[k]) && matrix[i + rowMove[k]][j + colMove[k]] != null) {
+                        count++;
+                    }
+                }
+
+            }
+        }
+        System.out.println("Number of edges: " + count);
     }
 
     public static void printGraph(GraphNode[][] matrix) {
@@ -43,6 +69,10 @@ public class FileIO {
             }
             System.out.println();
         }
+        System.out.println("Number of nodes: " + numberNodes);
+        System.out.println("Number of items: " + numberItems);
+        System.out.println("Number of holes: " + numberOfHoles);
+        getNumberOfEdges(matrix);
     }
 
 
@@ -76,15 +106,17 @@ public class FileIO {
         startTimer();
         DFS.printInfo(dfs, 0, 0, 0, true);
         stopTimer();
+        System.out.println("\nDijkstra: ");
+        GraphNode[][] dijkstra = GraphNode.copyMatrix(matrix);
+        startTimer();
+        Dijkstra.printInfo(dijkstra, 0, 0, true);
+        stopTimer();
         GraphNode[][] bell = GraphNode.copyMatrix(matrix);
         System.out.println("\nBellman-Ford: ");
         startTimer();
         BellmanFord.printInfo(bell, 0, 0, true);
         stopTimer();
-        System.out.println("\nDijkstra: ");
-        startTimer();
-        Dijkstra.printInfo(bell, 0, 0, true);
-        stopTimer();
+
 
     }
 }
